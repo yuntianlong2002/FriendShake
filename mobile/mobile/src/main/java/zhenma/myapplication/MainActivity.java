963bb75eb6a8017187af6b240019be87c6de9035
yuntianlong2002 <yuntianlong2002@gmail.com>
2016-02-24 22:06:27 -0500

    Users can login now

--- mobile/mobile/src/main/java/zhenma/myapplication/MainActivity.java
+++ mobile/mobile/src/main/java/zhenma/myapplication/MainActivity.java
@@ -9,11 +9,14 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
